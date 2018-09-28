a6730da34265f619752c045f24e79d4a50053621
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2018-09-28 22:08:38 +0530

    Add files via upload

---
+++ 18.stackarr.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void pop(int *i){
+	if (*i>0) (*i)--;
+	else printf("empty stack\n");		
+}
+
+void push(int a[],int x,int *i){
+	if (*i<10) a[(*i)++]=x;
+	else printf("full stack\n");
+}
+
+void print(int a[],int i){
+	int j;
+	for (j=0;j<i;j++) printf("%d ",a[j]);
+	printf("\n");
+}
+int main(){
+	int a[10],i=0,x,c;
+	do{
+	printf("1:pop\n2:push\n3:print\n4:exit\n");
+	scanf("%d",&c);
+	switch(c){
+		case 1: pop(&i); break;
+		case 2: scanf("%d",&x); push(a,x,&i); break;
+		case 3: print(a,i); break;
+	};
+	}while(c!=4);
+	return 0;
+}
---
+++ 19.stackll.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+struct Node{
+	int val;
+	struct Node *next;
+}*head,*ptr;
+
+int main(){
+	char x;
+	int i;
+	struct Node *head,*ptr;
+	head->next=NULL;
+	
+	do{
+		scanf("%c",&x);
+		switch(x){
+			case 's':
+				scanf("%d",&i);
+				ptr=head->next;
+				while (ptr!=NULL){ if (ptr->val==i) printf("found\n"); ptr=ptr->next;}
+				break;
+			case 'i':
+				scanf("%d",&i);
+				ptr=head;
+				while (ptr->next!=NULL) ptr=ptr->next;
+				ptr->next= (struct Node*)malloc(sizeof(struct Node));
+				ptr->next->val=i;
+				break;
+			case 'd':
+				ptr=head;
+				while (ptr->next->next!=NULL) ptr=ptr->next;
+				free(ptr->next);
+				ptr->next=NULL;
+				break;
+			case 'p':
+			
+				ptr=head->next;
+				while(ptr!=NULL){
+					printf("%d ",ptr->val);
+					ptr=ptr->next;
+				}
+				printf("\n");
+				break;
+		};
+	}while(x!='e');
+
+	return 0;
+}
